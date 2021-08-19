2066fc0a6fae43864b8c46c20a366e1ca656a29a
Kirill2602 <kaspicin2602@gmail.com>
2021-08-19 18:02:59 +0300

    commit

---
+++ e-shop/src/components/v-catalog-item.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="v-catalog-item">
+  <img :src=" require('../assets/img/'+product_data.image)" alt="pic">
+  <p class="v-catalog-item_name">{{product_data.name}}</p>
+  <p class="v-catalog-item_price">Price: {{product_data.price}} ₽</p>
+  <button class="v-catalog-item_add_to_cart_btn btn" @click="sendDataToParent">Add to cart</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "v-catalog-item",
+  props:{
+    product_data:{
+    type: Object,
+      default(){
+      return {}
+      }
+    },
+  },
+  methods:{
+    sendDataToParent(){
+      this.$emit('sendArticle', this.product_data.article);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-catalog-item{
+    flex-basis: 25%;
+    box-shadow: 0 0 8px 0 #e0e0e0;
+    padding: 16px;
+    margin-bottom: 16px;
+  }
+</style>
---
+++ e-shop/src/components/v-main-wrapper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="v-main-wrapper">
+    <v-catalog/>
+  </div>
+
+</template>
+
+<script>
+import vCatalog from './v-catalog'
+import vCart from './v-cart'
+
+
+export default {
+  name: "v-main-wrapper",
+  components: {vCatalog, vCart,},
+  watch:{
+
+  },
+  props:{
+  },
+  data(){
+    return{
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  mounted() {
+    console.log('Mounted')
+  }
+}
+</script>
+
+<style>
+  .v-main-wrapper{
+    display: flex;
+    margin: 0 auto;
+  }
+</style>
